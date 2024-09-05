7745c49cfe9a680bb9cf48835b15e91d239f07a9
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-09-05 12:10:46 -0500

    Task 2: Order Management System

--- cc_example.js
+++ cc_example.js
@@ -4,9 +4,19 @@ let customerName = "Bob Marley";
 const customerID = 1234;
 var isPremiumMember = false;
 
+// logging each variable's value and type to the console
 console.log(customerName, typeof(customerName));
 console.log(customerID, typeof(customerID));
 console.log(isPremiumMember,typeof(isPremiumMember));
 
+// Task 2: Compound Data Types
 
+let orderItems = ["Computer", "Laptop", "Macbook"]; // Declaring an array to store listed items
+const orderDetails = {   
+    orderID: 123, 
+    items: orderItems,
+    totalAmount: 2459.00
+                    } // Creating an object to represent an order with properties like OrderID, items, and totalAmount
+
+console.log(orderItems, orderDetails); // logging orderItems and orderDetails to the console
 
